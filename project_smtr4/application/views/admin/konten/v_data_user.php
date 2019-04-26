aed4629a4b1f5a33e2c2547136b0d3f785dee1a0
palindungan <rizkika.palindungan1@gmail.com>
2019-04-26 08:22:56 +0700

    Update v_data_user.php
    edit

--- project_smtr4/application/views/admin/konten/v_data_user.php
+++ project_smtr4/application/views/admin/konten/v_data_user.php
@@ -191,10 +191,7 @@
 <!-- untuk ajax -->
 <!-- <script src="<?= base_url(); ?>assets/template/back/js/jquery.min.js"></script> -->
 
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script>
-    window.jQuery || document.write('<script src="<?php echo base_url(); ?>assets/template/back/src/js/vendor/jquery-3.3.1.min.js"><\/script>')
-</script>
+<script src="<?php echo base_url(); ?>assets/template/back/src/js/vendor/jquery-3.3.1.min.js"></script>
 
 <!-- script logika -->
 <script type="text/javascript">
@@ -244,71 +241,91 @@
                     id_user: m
                 },
                 success: function(data) {
-                    let result = data.tbl_user;
+                    var obj = JSON.parse(data);
 
-                    alert(result);
+                    var owner = "";
+                    var admin = "";
+                    var kasir = "";
+                    if (obj['tbl_user'][0]['level'] == "owner") {
+                        owner = "selected";
+                    }
+                    if (obj['tbl_user'][0]['level'] == "admin") {
+                        admin = "selected";
+                    }
+                    if (obj['tbl_user'][0]['level'] == "kasir") {
+                        kasir = "selected";
+                    }
 
-                    //     $('.isi_modal_edit').html(`
+                    var pria = "";
+                    var wanita = "";
+                    if (obj['tbl_user'][0]['jenkel_user'] == "pria") {
+                        pria = "selected";
+                    }
+                    if (obj['tbl_user'][0]['jenkel_user'] == "wanita") {
+                        wanita = "selected";
+                    }
 
-                    //     <div class="form-group row">
-                    //         <label for="exampleInput1" class="col-sm-3 col-form-label">Kode User</label>
-                    //         <div class="col-sm-9">
-                    //             <input name="id_user" type="text" class="form-control form_data1" id="exampleInput1" value="` + data.tbl_user + `">
-                    //         </div>
-                    //     </div>
-                    //     <div class="form-group row">
-                    //         <label for="exampleInput2" class="col-sm-3 col-form-label">Nama</label>
-                    //         <div class="col-sm-9">
-                    //             <input name="nm_user" type="text" class="form-control form_data1" id="exampleInput2" placeholder="Nama">
-                    //         </div>
-                    //     </div>
-                    //     <div class="form-group row">
-                    //         <label for="exampleInput3" class="col-sm-3 col-form-label">Alamat</label>
-                    //         <div class="col-sm-9">
-                    //             <textarea name="almt_user" class="form-control form_data1" id="exampleInput3" rows="3" placeholder="Alamat"></textarea>
-                    //         </div>
-                    //     </div>
-                    //     <div class="form-group row">
-                    //         <label for="exampleInput4" class="col-sm-3 col-form-label">Jenis Kelamin</label>
-                    //         <div class="col-sm-9">
-                    //             <select name="jenkel_user" class="form-control form_data1" id="exampleInput4">
-                    //                 <option value="">-</option>
-                    //                 <option value="pria">Pria</option>
-                    //                 <option value="wanita">Wanita</option>
-                    //             </select>
-                    //         </div>
-                    //     </div>
-                    //     <div class="form-group row">
-                    //         <label for="exampleInput5" class="col-sm-3 col-form-label">Username</label>
-                    //         <div class="col-sm-9">
-                    //             <input name="username" type="text" class="form-control form_data1" id="exampleInput5" placeholder="Username">
-                    //         </div>
-                    //     </div>
-                    //     <div class="form-group row">
-                    //         <label for="exampleInput6" class="col-sm-3 col-form-label">Password</label>
-                    //         <div class="col-sm-9">
-                    //             <input name="password" type="password" class="form-control form_data1" id="exampleInput6" placeholder="Password">
-                    //         </div>
-                    //     </div>
-                    //     <div class="form-group row">
-                    //         <label for="exampleInput7" class="col-sm-3 col-form-label">Konfirmasi Password</label>
-                    //         <div class="col-sm-9">
-                    //             <input type="password" class="form-control form_data1" id="exampleInput7" placeholder="Konfirmasi Password">
-                    //         </div>
-                    //     </div>
-                    //     <div class="form-group row">
-                    //         <label for="exampleInput8" class="col-sm-3 col-form-label">Level</label>
-                    //         <div class="col-sm-9">
-                    //             <select name="level" id="exampleInput8" name="level" class="form-control form_data1">
-                    //                 <option value="">-</option>
-                    //                 <option value="owner">Owner</option>
-                    //                 <option value="admin">Admin</option>
-                    //                 <option value="kasir">Kasir</option>
-                    //             </select>
-                    //         </div>
-                    //     </div>
+                    $('.isi_modal_edit').html(`
 
-                    // `);
+                        <div class="form-group row">
+                            <label for="exampleInput1" class="col-sm-3 col-form-label">Kode User</label>
+                            <div class="col-sm-9">
+                                <input name="id_user" type="text" class="form-control form_data1" id="exampleInput1" readonly="" value="` + obj['tbl_user'][0]['id_user'] + `">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInput2" class="col-sm-3 col-form-label">Nama</label>
+                            <div class="col-sm-9">
+                                <input name="nm_user" type="text" class="form-control form_data1" id="exampleInput2" placeholder="Nama" value="` + obj['tbl_user'][0]['nm_user'] + `">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInput3" class="col-sm-3 col-form-label">Alamat</label>
+                            <div class="col-sm-9">
+                                <textarea name="almt_user" class="form-control form_data1" id="almt_user1" rows="3" placeholder="Alamat">` + obj['tbl_user'][0]['almt_user'] + `</textarea>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInput4" class="col-sm-3 col-form-label">Jenis Kelamin</label>
+                            <div class="col-sm-9">
+                                <select name="jenkel_user" class="form-control form_data1" id="exampleInput4">
+                                    <option value="">-</option>
+                                    <option value="pria" ` + pria + `>Pria</option>
+                                    <option value="wanita" ` + wanita + `>Wanita</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInput5" class="col-sm-3 col-form-label">Username</label>
+                            <div class="col-sm-9">
+                                <input name="username" type="text" class="form-control form_data1" id="exampleInput5" placeholder="Username" value="` + obj['tbl_user'][0]['username'] + `">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInput6" class="col-sm-3 col-form-label">Password</label>
+                            <div class="col-sm-9">
+                                <input name="password" type="password" class="form-control form_data1" id="exampleInput6" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInput7" class="col-sm-3 col-form-label">Konfirmasi Password</label>
+                            <div class="col-sm-9">
+                                <input type="password" class="form-control form_data1" id="exampleInput7" placeholder="Konfirmasi Password">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="exampleInput8" class="col-sm-3 col-form-label">Level</label>
+                            <div class="col-sm-9">
+                                <select name="level" id="exampleInput8" name="level" class="form-control form_data1">
+                                    <option value="">-</option>
+                                    <option value="owner" ` + owner + `>Owner</option>
+                                    <option value="admin" ` + admin + `>Admin</option>
+                                    <option value="kasir" ` + kasir + `>Kasir</option>
+                                </select>
+                            </div>
+                        </div>
+
+                    `);
                 }
             });
             // ajax

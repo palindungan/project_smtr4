92ffec2d15e9d7759752966ea65edb994b76a3e6
palindungan <rizkika.palindungan1@gmail.com>
2019-04-29 14:56:40 +0700

    upload image
    upload 1 image

--- project_smtr4/application/controllers/admin/menu/Data_Menu.php
+++ project_smtr4/application/controllers/admin/menu/Data_Menu.php
@@ -8,4 +8,19 @@ class Data_Menu extends CI_Controller
         $data['path'] = 'admin/konten/menu/v_data_menu';
         $this->load->view('admin/_view', $data);
     }
+
+    function ajax_upload()
+    {
+        if (isset($_FILES['image_file']['name'])) {
+            $config['upload_path'] = './upload/';
+            $config['allowed_types'] = 'jpg|jpeg|png|gif';
+            $this->load->library('upload', $config);
+            if (!$this->upload->do_upload('image_file')) {
+                echo $this->upload->display_errors();
+            } else {
+                $data = $this->upload->data();
+                echo '<img src="' . base_url() . 'upload/' . $data["file_name"] . '" />';
+            }
+        }
+    }
 }
--- project_smtr4/application/views/admin/konten/menu/v_data_menu.php
+++ project_smtr4/application/views/admin/konten/menu/v_data_menu.php
@@ -8,8 +8,8 @@
                     <div class="page-header-title">
                         <i class="ik ik-command bg-blue"></i>
                         <div class="d-inline">
-                            <h5>Data Menu</h5>
-                            <span>ini adalah deksripsi menu Data Menu</span>
+                            <h2>Data Menu Makanan</h2>
+                            <!-- <span>ini adalah deksripsi menu user</span> -->
                         </div>
                     </div>
                 </div>
@@ -17,15 +17,16 @@
                     <nav class="breadcrumb-container" aria-label="breadcrumb">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item">
-                                <a href="../../index.html"><i class="ik ik-home"></i></a>
+                                <a href="<?php echo base_url(); ?>admin/home/"><i class="ik ik-home"></i></a>
                             </li>
                             <li class="breadcrumb-item"><a href="">UI</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Data Menu</li>
+                            <li class="breadcrumb-item active" aria-current="page">Data Menu Makanan</li>
                         </ol>
                     </nav>
                 </div>
             </div>
         </div>
+
 
         <!-- bagian ISI KONTEN -->
         <div class="card">
@@ -35,6 +36,20 @@
             <div class="card-body">
 
                 <!-- disini isinya konten -->
+
+                <form action="" method="post" id="upload_form">
+                    <input type="file" name="image_file" id="image_file">
+                    <br />
+                    <br />
+                    <input type="submit" name="upload" id="upload" value="Upload">
+                </form>
+                <br />
+                <br />
+
+                <div id="uploaded_image">
+
+                </div>
+
 
             </div>
         </div>
@@ -46,3 +61,32 @@
 <!-- <script src="<?= base_url(); ?>assets/template/back/js/jquery.min.js"></script> -->
 
 <script src="<?php echo base_url(); ?>assets/template/back/src/js/vendor/jquery-3.3.1.min.js"></script>
+
+<!-- script logika -->
+<script type="text/javascript">
+    $(document).ready(function() {
+
+        $('#upload_form').on('submit', function(e) {
+            e.preventDefault();
+            if ($('#image_file').val() == '') {
+                alert("please select the file");
+            } else {
+                $.ajax({
+                    url: "<?php echo base_url(); ?>admin/menu/data_menu/ajax_upload",
+                    method: "post",
+                    data: new FormData(this),
+                    contentType: false,
+                    cache: false,
+                    processData: false,
+                    success: function(data) {
+
+                        $('#uploaded_image').html(data);
+
+                    }
+                });
+            }
+        });
+
+    });
+</script>
+<!-- script logika -->

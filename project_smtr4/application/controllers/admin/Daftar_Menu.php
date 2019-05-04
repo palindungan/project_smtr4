823938770a5d9eff5eb7b0c656d7707088b4ca3c
palindungan <rizkika.palindungan1@gmail.com>
2019-05-05 01:30:43 +0700

    penyempurnaan code pencarian (modal)
    penyempurnaan code pencarian (modal)

--- project_smtr4/application/controllers/admin/Daftar_Menu.php
+++ project_smtr4/application/controllers/admin/Daftar_Menu.php
@@ -27,19 +27,7 @@ class Daftar_Menu extends CI_Controller
         $search_input = $this->input->post('search_input');
         $kategori = $this->input->post('kategori');
 
-        if ($kategori == "All Kategori" && $search_input == "") {
-            // pencarian semua data
-            $data_cari['tbl_data'] = $this->M_daftar_menu->cari_data_semua()->result();
-        } elseif ($kategori == "All Kategori" && $search_input != "") {
-            // pencarian semua yang sesuai search_input
-            $data_cari['tbl_data'] = $this->M_daftar_menu->cari_data_by_input($search_input)->result();
-        } elseif ($kategori != "All Kategori" && $search_input == "") {
-            // pencarian semua yang sesuai kategori
-            $data_cari['tbl_data'] = $this->M_daftar_menu->cari_data_by_kat($kategori)->result();
-        } else {
-            // pencarian berdasarkan kategori & search_input
-            $data_cari['tbl_data'] = $this->M_daftar_menu->cari_data_by_kat_input($search_input, $kategori)->result();
-        }
+        $data_cari['tbl_data'] = $this->M_daftar_menu->cari_data($search_input, $kategori)->result();
 
         $data = json_encode($data_cari);
 
--- project_smtr4/application/models/admin/M_daftar_menu.php
+++ project_smtr4/application/models/admin/M_daftar_menu.php
@@ -7,53 +7,47 @@ class M_daftar_menu extends CI_Model
         return $this->db->get('tbl_kategori');
     }
 
-    // pencarian semua data
-    function cari_data_semua()
+    // pencarian data
+    function cari_data($search_input, $kategori)
     {
-        $this->db->select('*');
-        $this->db->from('tabel_menu tm');
+        if ($kategori == "All Kategori" && $search_input == "") {
+            // pencarian semua data
+            $this->db->select('*');
+            $this->db->from('tabel_menu tm');
 
-        $this->db->order_by('tm.nm_menu', 'ASC');
+            $this->db->order_by('tm.nm_menu', 'ASC');
 
-        return $this->db->get();
-    }
+            return $this->db->get();
+        } elseif ($kategori == "All Kategori" && $search_input != "") {
+            // pencarian semua yang sesuai search_input
+            $this->db->select('*');
+            $this->db->from('tabel_menu tm');
 
-    // pencarian data dengan 2 parameter
-    function cari_data_by_kat_input($search_input, $kategori)
-    {
-        $this->db->select('*');
-        $this->db->from('tabel_menu tm');
+            $where = "tm.nm_menu ='" . $search_input . "'";
+            $this->db->where($where);
+            $this->db->order_by('tm.nm_menu', 'ASC');
 
-        $where = "tm.nm_menu ='" . $search_input . "' && tm.id_kat ='" . $kategori . "'";
-        $this->db->where($where);
-        $this->db->order_by('tm.nm_menu', 'ASC');
+            return $this->db->get();
+        } elseif ($kategori != "All Kategori" && $search_input == "") {
+            // pencarian semua yang sesuai kategori
+            $this->db->select('*');
+            $this->db->from('tabel_menu tm');
 
-        return $this->db->get();
-    }
+            $where = "tm.id_kat ='" . $kategori . "'";
+            $this->db->where($where);
+            $this->db->order_by('tm.nm_menu', 'ASC');
 
-    // pencarian data dengan 1 parameter
-    function cari_data_by_kat($kategori)
-    {
-        $this->db->select('*');
-        $this->db->from('tabel_menu tm');
+            return $this->db->get();
+        } else {
+            // pencarian berdasarkan kategori & search_input
+            $this->db->select('*');
+            $this->db->from('tabel_menu tm');
 
-        $where = "tm.id_kat ='" . $kategori . "'";
-        $this->db->where($where);
-        $this->db->order_by('tm.nm_menu', 'ASC');
+            $where = "tm.nm_menu ='" . $search_input . "' && tm.id_kat ='" . $kategori . "'";
+            $this->db->where($where);
+            $this->db->order_by('tm.nm_menu', 'ASC');
 
-        return $this->db->get();
-    }
-
-    // pencarian data dengan 1 parameter
-    function cari_data_by_input($search_input)
-    {
-        $this->db->select('*');
-        $this->db->from('tabel_menu tm');
-
-        $where = "tm.nm_menu ='" . $search_input . "'";
-        $this->db->where($where);
-        $this->db->order_by('tm.nm_menu', 'ASC');
-
-        return $this->db->get();
+            return $this->db->get();
+        }
     }
 }

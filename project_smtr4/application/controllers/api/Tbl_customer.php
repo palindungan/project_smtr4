51e6f8118df393314230e11d90726723081f01a8
palindungan <rizkika.palindungan1@gmail.com>
2019-05-10 14:21:36 +0700

    Update Tbl_customer.php
    api

--- project_smtr4/application/controllers/api/Tbl_customer.php
+++ project_smtr4/application/controllers/api/Tbl_customer.php
@@ -26,5 +26,62 @@ class Tbl_customer extends REST_Controller
         $this->response($tbl_customer, 200);
     }
 
-    //Masukan function selanjutnya disini
+    //Mengirim atau menambah data kontak baru
+    function index_post()
+    {
+        $data = array(
+            'id_customer'           => $this->post('id_customer'),
+            'nm_customer'          => $this->post('nm_customer'),
+            'almt_customer'    => $this->post('almt_customer'),
+            'jenkel_customer'           => $this->post('jenkel_customer'),
+            'no_hp'          => $this->post('no_hp'),
+            'email'    => $this->post('email'),
+            'username'           => $this->post('username'),
+            'password'          => $this->post('password')
+        );
+
+        $insert = $this->db->insert('tbl_customer', $data);
+        if ($insert) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+
+    //Memperbarui data kontak yang telah ada
+    function index_put()
+    {
+        $id_customer = $this->put('id_customer');
+        $data = array(
+            'id_customer'           => $this->put('id_customer'),
+            'nm_customer'          => $this->put('nm_customer'),
+            'almt_customer'    => $this->put('almt_customer'),
+            'jenkel_customer'           => $this->put('jenkel_customer'),
+            'no_hp'          => $this->put('no_hp'),
+            'email'    => $this->put('email'),
+            'username'           => $this->put('username'),
+            'password'          => $this->put('password')
+        );
+
+        $this->db->where('id_customer', $id_customer);
+        $update = $this->db->update('tbl_customer', $data);
+        if ($update) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+
+    //Menghapus salah satu data kontak
+    function index_delete()
+    {
+        $id_customer = $this->delete('id_customer');
+        $this->db->where('id_customer', $id_customer);
+        $delete = $this->db->delete('tbl_customer');
+        if ($delete) {
+            $this->response(array('status' => 'success'), 201);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
 }

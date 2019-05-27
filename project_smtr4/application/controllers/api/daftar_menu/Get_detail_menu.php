f3d36c45edcb5af868981bf2c1a182561d8effe6
palindungan <rizkika.palindungan1@gmail.com>
2019-05-28 05:21:35 +0700

    Create Get_detail_menu.php
    API untuk gambar dari server ke android

---
+++ project_smtr4/application/controllers/api/daftar_menu/Get_detail_menu.php
@@ -0,0 +1,69 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
+
+class Get_detail_menu extends REST_Controller
+{
+
+    function __construct($config = 'rest')
+    {
+        parent::__construct($config);
+        $this->load->database();
+        $this->load->model("admin/menu/M_data_menu");
+    }
+
+    //mengirim data detail
+    function index_post()
+    {
+
+        // mengambil jumlah baris
+        $cek = $this->M_data_menu->tampil_data()->num_rows();
+
+        // cek apakah ada data dari username
+        if ($cek > 0) {
+
+            // mengambil data dari database berdasarkan username
+            $query = $this->M_data_menu->tampil_data();
+
+            // variable array
+            $result = array();
+            $result['daftar_menu'] = array();
+
+            // mengeluarkan data dari database
+            foreach ($query->result_array() as $row) {
+
+                // mengambil gambar dan mengkonversi kedalam string untuk dikirim API
+                $path = "./upload/gambar_menu/";
+                $file = $path . $row["gambar"];
+
+                $encode = base64_encode(file_get_contents($file));
+
+                // kumpulan data
+                $data = array(
+                    'id_menu' => $row["id_menu"],
+                    'nm_menu' => $row["nm_menu"],
+                    'nm_kat' => $row["nm_kat"],
+                    'tipe' => $row["tipe"],
+                    'hrg_porsi' => $row["hrg_porsi"],
+                    'gambar' => $encode,
+                    'deskripsi' => $row["deskripsi"],
+                    'id_kat' => $row["id_kat"]
+                );
+
+                array_push($result['daftar_menu'], $data);
+
+                // membuat array untuk di transfer
+                $result["success"] = "1";
+                $result["message"] = "success";
+                $this->response($result, 200);
+            }
+        } else {
+            // membuat array untuk di transfer ke API
+            $result["success"] = "0";
+            $result["message"] = "error username";
+            $this->response($result, 404);
+        }
+    }
+}

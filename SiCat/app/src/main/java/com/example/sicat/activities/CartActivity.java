06ecb6aaf7ac648529d983af972ea1ca4760bbf7
palindungan <rizkika.palindungan1@gmail.com>
2019-06-19 17:12:57 +0700

    layout formorder
    layout formorder

--- SiCat/app/src/main/java/com/example/sicat/activities/CartActivity.java
+++ SiCat/app/src/main/java/com/example/sicat/activities/CartActivity.java
@@ -1,6 +1,8 @@
 package com.example.sicat.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.sicat.Database.ModelDB.Cart;
@@ -59,6 +62,12 @@ public class CartActivity extends AppCompatActivity {
         recycler_cart.setHasFixedSize(true);
 
         btn_place_order = (Button)findViewById(R.id.btn_place_order);
+        btn_place_order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
 
         loadCartItems();
 
@@ -144,5 +153,34 @@ public class CartActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         loadCartItems();
+    }
+
+    private void showDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        // set title dialog
+        alertDialogBuilder.setTitle("Ingin Melakukan Pemesanan ?");
+
+        // set pesan dari dialog
+        alertDialogBuilder
+                .setMessage("Klik Ya untuk melakukan pemesanan !")
+                .setPositiveButton("Ya",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // masuk form inputan transaksi pemesanan
+
+                    }
+                })
+                .setNegativeButton("Tidak",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        // membuat alert dialog dari builder
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // menampilkan alert dialog
+        alertDialog.show();
     }
 }
---
+++ SiCat/app/src/main/java/com/example/sicat/activities/FormOrderActivity.java
@@ -0,0 +1,15 @@
+package com.example.sicat.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sicat.R;
+
+public class FormOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_order);
+    }
+}

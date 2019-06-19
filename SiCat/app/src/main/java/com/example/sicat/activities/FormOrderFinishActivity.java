ff08617a8fb5673f7e472f31102a623963800105
palindungan <rizkika.palindungan1@gmail.com>
2019-06-19 17:59:54 +0700

    update layout
    update layout

---
+++ SiCat/app/src/main/java/com/example/sicat/activities/FormOrderFinishActivity.java
@@ -0,0 +1,15 @@
+package com.example.sicat.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sicat.R;
+
+public class FormOrderFinishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_order_finish);
+    }
+}

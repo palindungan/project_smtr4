c39ee0ae3b508592babdd996e7cd48730331ccde
palindungan <rizkika.palindungan1@gmail.com>
2019-07-07 18:47:46 +0700

    Update head.php
    pembenaran judul web

--- project_smtr4/application/views/_partials/head.php
+++ project_smtr4/application/views/_partials/head.php
@@ -1,11 +1,11 @@
 <meta charset="utf-8">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
-<title>Icons | ThemeKit - Admin Template</title>
+<title><?php echo $this->uri->segment('2') ?></title>
 <meta name="description" content="">
 <meta name="keywords" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="icon" href="<?php echo base_url(); ?>assets/template/back/favicon.ico" type="image/x-icon" />
+<!-- <link rel="icon" href="<?php echo base_url(); ?>assets/template/back/favicon.ico" type="image/x-icon" /> -->
 
 <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
 

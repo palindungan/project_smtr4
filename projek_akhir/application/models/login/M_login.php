daa2d6a1e19834fa9916f08ad08bdef57aafe8df
rakhmatfadilah <rakhmatfadilah2@gmail.com>
2019-07-10 02:29:12 +0700

    api nya
    api nya

--- projek_akhir/application/models/login/M_login.php
+++ projek_akhir/application/models/login/M_login.php
@@ -12,4 +12,16 @@ class M_login extends CI_Model
 
         return $this->db->get();
     }
+
+    function ambil_data2($username)
+    {
+        $this->db->select('*');
+        $this->db->from('siswa u');
+
+        $where = "u.username ='" . $username . "'";
+        $this->db->where($where);
+        $this->db->order_by('u.username', 'ASC');
+
+        return $this->db->get();
+    }
 }
